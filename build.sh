184a56660b29f2e7f7b9e4c724e9585a869657c2
root <root@URBK.bcs.local>
2025-05-28 00:11:55 +0100

    Initial commit with multi-platform Docker support

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+# Set your Docker Hub username
+DOCKER_USERNAME="hazemhlawa"
+IMAGE_NAME="dbm"
+VERSION="1.0.0"
+
+# Create builder instance if it doesn't exist
+docker buildx create --name dbm-builder --use || true
+
+# Build and push linux/amd64 image
+docker buildx build --platform linux/amd64 \
+    -t "${DOCKER_USERNAME}/${IMAGE_NAME}:latest" \
+    -t "${DOCKER_USERNAME}/${IMAGE_NAME}:${VERSION}" \
+    --push \
+    .
+
+echo "linux/amd64 build completed and pushed to Docker Hub"
+
