c9e0d89ef1cd1adc28ff67613f20b77abb2d10cc
root <root@URBK.bcs.local>
2025-05-28 09:39:51 +0100

    Implement multi-database monitoring support (v2)

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ set -e
 # Set your Docker Hub username
 DOCKER_USERNAME="hazemhlawa"
 IMAGE_NAME="dbm"
-VERSION="1.0.0"
+VERSION="2"
 
 # Create builder instance if it doesn't exist
-docker buildx create --name dbm-builder --use || true
+#docker buildx create --name dbm-builder --use || true
 
 # Remove any existing builder to avoid caching issues
 docker buildx rm dbm-builder || true
